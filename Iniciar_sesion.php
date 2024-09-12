647be536a69f45312d66da319c2a31d669d93d6e
nicomazzag <nicolasmazzaglia.nm@gmail.com>
2024-09-12 17:34:17 -0300

    Css Iniciar session

--- Iniciar_sesion.php
+++ Iniciar_sesion.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="iniciar_sesion.css">
+    <link rel="stylesheet" href="iniciarSession.css">
     <title>Logueo</title>
 </head>
 <body>
-    <form style="align-items: center; margin: auto; width: 25%;" action="" class="form">
+    <form style="align-items: center; margin: auto;" action="" class="form">
         <p>
             Bienvenido,<span>Iniciar sesión para continuar</span>
         </p>
-        <img src="https://thumbs.dreamstime.com/b/%C3%ADcone-de-usu%C3%A1rio-m%C3%ADdia-social-vetor-imagem-perfil-do-avatar-padr%C3%A3o-retrato-182347582.jpg" alt="Logo de usuario" class="logo_de_usuario"  title="Logo de usuario">
+        <!-- <img src="https://thumbs.dreamstime.com/b/%C3%ADcone-de-usu%C3%A1rio-m%C3%ADdia-social-vetor-imagem-perfil-do-avatar-padr%C3%A3o-retrato-182347582.jpg" alt="Logo de usuario" class="logo_de_usuario"  title="Logo de usuario"> -->
         <label for="email"></label>
         <input type="email" placeholder="Email" name="email" id="email" required>
         <label for="password"></label>
@@ -26,12 +26,12 @@
             <span>O</span>
             <div></div>
         </div>
-                    <p>¿Quiéres registrarse?
-                        <a href="registrarse_nuevo_usuario.php">Registrarse</a>
-                    </p>
-                    <p>¿Olvidaste tu contraeña?
-                        <a href="recuperar_contra.php">Recuperar contraseña</a>
-                    </p>
-</form>
+        <p>¿Quiéres registrarse?
+            <a href="registrarse_nuevo_usuario.php">Registrarse</a>
+        </p>
+        <p>¿Olvidaste tu contraeña?
+            <a href="recuperar_contra.php">Recuperar contraseña</a>
+        </p>
+    </form>
 </body>
 </html
