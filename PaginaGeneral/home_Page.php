8f6dfa8648ef05e65671ceef6d2d5feb0c33f9f7
nicomazzag <nicolasmazzaglia.nm@gmail.com>
2024-09-22 01:48:36 -0300

    Promociones (mejorar)

--- PaginaGeneral/home_Page.php
+++ PaginaGeneral/home_Page.php
@@ -23,6 +23,7 @@
     <?php 
         include("../Include/header.php");
     ?>
+    <!-- Introduccion -->
     <div class="conteiner numero1">
     <div class="row">
     <div class="col-md-8 col-12">
@@ -97,10 +98,76 @@
             </div>
         </div>
     </div>
-    <!-- Parte de la paginación de promociones -->
-    <div class="conteiner numero2" style="background-color: red;" > <!--eliminar style -->
+    <!-- Promociones -->
+    <div class="conteiner numero2 espacio"> 
         <div class="row">
-            <h2>Promociones</h2>
+            <div class="col-md-6">
+                <div class="conteiner promociones">
+                    <div class="row">
+                        <div class="col-12">
+                            <h1 class="titulo">Promociones</h1>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <p>Descubre las mejores promociones de Zorzal Centro Comercial!!</p>
+                            <p>Nuestras mejores promociones del momento. No dejes pasar la oportunidad!!</p>
+                            <div class="centrar">
+                                <i class="fas fa-arrow-right "></i>
+                                <i class="fas fa-arrow-right "></i>
+                                <i class="fas fa-arrow-right "></i>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6" >
+                <div class="conteiner ">
+                    <div class="row">
+                        <div class="col-12">
+                            <div id="carouselExampleCaptions" class="carousel slide">
+                              <div class="carousel-indicators">
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                              </div>
+                              <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                  <img src="https://th.bing.com/th/id/OIP.BwLjKdv-fdzPs2vOwIzpBAHaJL?pid=ImgDet&w=202&h=250&c=7&dpr=1,1" class=" w-100 imgPromos" alt="promocion">
+                                  <div class="carousel-caption d-none d-md-block">
+                                    <h5>First slide label</h5>
+                                    <p>Some representative placeholder content for the first slide.</p>
+                                  </div>
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="https://th.bing.com/th/id/OIP.BwLjKdv-fdzPs2vOwIzpBAHaJL?pid=ImgDet&w=202&h=250&c=7&dpr=1,1" class=" w-100 imgPromos" alt="promocion">
+                                  <div class="carousel-caption d-none d-md-block">
+                                    <h5>Second slide label</h5>
+                                    <p>Some representative placeholder content for the second slide.</p>
+                                  </div>
+                                </div>
+                                <div class="carousel-item">
+                                  <img src="https://th.bing.com/th/id/OIP.BwLjKdv-fdzPs2vOwIzpBAHaJL?pid=ImgDet&w=202&h=250&c=7&dpr=1,1" class=" w-100 imgPromos" alt="promocion">
+                                  <div class="carousel-caption d-none d-md-block">
+                                    <h5>Third slide label</h5>
+                                    <p>Some representative placeholder content for the third slide.</p>
+                                  </div>
+                                </div>
+                              </div>
+                              <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Previous</span>
+                              </button>
+                              <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="visually-hidden">Next</span>
+                              </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
     </div>
     <!-- Nuestros locales -->
