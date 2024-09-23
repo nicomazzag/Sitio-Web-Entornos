9bf9b44294aabd96afbb87b086c0c688a494e20e
Franco Sussi <franconicolassussi@gmail.com>
2024-09-23 18:57:06 -0300

    .

--- PaginaAdmin/PrincipalAdmin.php
+++ PaginaAdmin/PrincipalAdmin.php
@@ -17,6 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&family=Protest+Guerrilla&display=swap" rel="stylesheet">
     
+    
     <title>Principal</title>
 </head>
 <body>
