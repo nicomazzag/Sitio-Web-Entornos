76945779301f2e7dd021a7ec0396538d9a126908
Pedro-MorettiYrure <114883924+Pedro-MorettiYrure@users.noreply.github.com>
2024-12-01 23:32:24 -0300

    Add files via upload

--- PaginaGeneral/Promociones.php
+++ PaginaGeneral/Promociones.php
@@ -1,14 +1,20 @@
-<?php 
-    include("../BasesDeDatos/UnicaBaseDeDatos.php");
-?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include_once("../Include/librerias.php");?>
+    <!-- Conexion con bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Conexion con font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <!-- Conexion con hojas de estilo -->
     <link rel="stylesheet" href="Promociones.css">
+    <!-- Conexion con el icono de la pagina -->
+    <link rel="icon" href="../Iconos/Logo_Shopping_Blanco.ico" type="image/x-icon">
+    <!-- Conexion con el google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&family=Protest+Guerrilla&display=swap" rel="stylesheet">
     <title>Promociones</title>
     <style>
         /*Hoja de estilo del footer(En caso de usar el footer en otras paginas (agregar los estilos en la hoja de estilos correspondiente)*/
@@ -36,18 +42,18 @@
         </div>
         <nav class="navbar bg-body-tertiary">
             <div class="container-fluid posicionar  mt-3 mb-3">
-                <form id="agrandar" class="d-flex" role="search">
-                    <input class="form-control me-2" type="search" placeholder="Buscar por nombre o código del local" aria-label="Buscar">
-                    <button id="espaciar" class="btn btn-outline-primary" type="submit">Search</button>
+                <form id="agrandar" class="d-flex" role="search" method="get" action="Promociones.php">
+                    <input class="form-control me-2" type="search" placeholder="Buscar por nombre o código del local" aria-label="Buscar" name="buscarTermino">
+                    <button id="espaciar" class="btn btn-outline-primary" type="submit">Buscar</button>
                     <div class="dropdown">
                         <button class="btn btn-outline-primary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Filtrar por
+                            Filtrar por categoría
                         </button>
-                      <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Premium</a></li>
-                            <li><a class="dropdown-item" href="#">Medium</a></li>
-                            <li><a class="dropdown-item" href="#">Inicial</a></li>
-                      </ul>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="?categoria=premium">Premium</a></li>
+                            <li><a class="dropdown-item" href="?categoria=medium">Medium</a></li>
+                            <li><a class="dropdown-item" href="?categoria=inicial">Inicial</a></li>
+                        </ul>
                     </div>
                 </form>
             </div>
@@ -58,29 +64,39 @@
             <div class="row">
                 <!-- Abrir base de datos -->
                 <?php
+                    include("../BasesDeDatos/UnicaBaseDeDatos.php");
+
+                    date_default_timezone_set('America/Argentina/Buenos_Aires');
+                    $dia_actual = date('w');
+
+                    $buscarTermino = isset($_GET['buscarTermino']) ? $_GET['buscarTermino'] : '';
+                    $categoria = isset($_GET['categoria']) ? $_GET['categoria'] : '';
+                    
                     $sql = "SELECT promociones.id, promociones.nombre, promociones.descripcion, promociones.categoriaMin, locales.nombre AS local_nombre FROM promociones 
-                    JOIN locales ON promociones.localid = locales.id";
+                            JOIN locales ON promociones.codLocal = locales.id WHERE
+                            SUBSTRING(diasValidos, $dia_actual + 1, 1) = '1' AND
+                            fechaDesde <= CURDATE() AND fechaHasta >= CURDATE() AND promociones.estadoPromo = 'aprobada'
+                            ORDER BY promociones.id";
+                    
+                    // Agregar condición de búsqueda si se ha ingresado un término
+                    if (!empty($buscarTermino)) {
+                        $buscarTermino = $conn->real_escape_string($buscarTermino);
+                        $sql .= " AND (locales.nombre LIKE '%$buscarTermino%' OR promociones.nombre LIKE '%$buscarTermino%' OR promociones.id LIKE '%$buscarTermino%')";
+                    }
+                    if(!empty($categoria)){
+                        $sql .= " AND promociones.categoriaMin = '$categoria'";
+                    }
                     $result = $conn->query($sql);
 
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
-                            switch ($row['categoriaMin']) {
-                                case '0':
-                                    $cat = 'Inicial';
-                                    break;
-                                case '1':
-                                    $cat = 'Medium';
-                                    break;
-                                case '2':
-                                    $cat = 'Premium';
-                                    break;                        
-                            }
+                            
                             echo '
                         <div class="col-12 col-sm-6 col-md-3 mt-2 mb-3">
                             <div class="card h-100">
                                 <div class="card-body">
                                 <h5 class="card-title">' . $row["nombre"] . '</h5>
-                                <strong><i>'. $cat . '</i></strong>
+                                <strong><i>'. $row['categoriaMin'] . '</i></strong>
                                 <p class="card-text">' . $row["descripcion"] . '</p>
                                 </div>
                                 <div class="card-footer">
--- PaginaGeneral/detallesLocal.php
+++ PaginaGeneral/detallesLocal.php
@@ -3,9 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include_once("../Include/librerias.php");?>
+    <!-- Conexion con bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- Conexion con font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <!-- Conexion con hojas de estilo -->
     <link rel="stylesheet" href="Promociones.css">
+    <!-- Conexion con el icono de la pagina -->
+    <link rel="icon" href="../Iconos/Logo_Shopping_Blanco.ico" type="image/x-icon">
+    <!-- Conexion con el google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@400;700&family=Protest+Guerrilla&display=swap" rel="stylesheet">
     <title>Promociones</title>
     <style>
         /*Hoja de estilo del footer(En caso de usar el footer en otras paginas (agregar los estilos en la hoja de estilos correspondiente)*/
@@ -26,7 +35,7 @@
         <?php 
             include("../Include/header.php");
 
-            include("../BasesDeDatos/BaseDeDatos_Locales.php");
+            include("../BasesDeDatos/UnicaBaseDeDatos.php");
 
         // Obtener el id del local desde la URL
         $local_id = isset($_GET['id']) ? intval($_GET['id']) : 0;
@@ -62,32 +71,20 @@
             <div class="container mb-3">
                 <div class="row">';
                 
-                include("../BasesDeDatos/BaseDeDatos_Locales.php");
+                //include("../BasesDeDatos/UnicaBaseDeDatos.php");
                 $sql = "SELECT promociones.id, promociones.nombre, promociones.descripcion, promociones.categoriaMin, locales.nombre AS local_nombre FROM promociones 
-                JOIN locales ON promociones.localid = locales.id WHERE locales.id = $local_id";
+                JOIN locales ON promociones.codLocal = locales.id WHERE locales.id = $local_id";
 
                 $result = $conn->query($sql);
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
-
-                            switch ($row['categoriaMin']) {
-                                case '0':
-                                    $cat = 'Inicial';
-                                    break;
-                                case '1':
-                                    $cat = 'Medium';
-                                    break;
-                                case '2':
-                                    $cat = 'Premium';
-                                    break;                        
-                            }
 
                             echo '
                         <div class="col-12 col-sm-6 col-md-3 mt-2 mb-3">
                             <div class="card h-100">
                                 <div class="card-body">
                                 <h5 class="card-title">' . $row["nombre"] . '</h5>
-                                <strong><i>'. $cat . '</i></strong>
+                                <strong><i>'. $row['categoriaMin']   . '</i></strong>
                                 <p class="card-text">' . $row["descripcion"] . '</p>
                                 </div>
                                 <div class="card-footer">
